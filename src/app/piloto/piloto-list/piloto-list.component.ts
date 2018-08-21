654e20a2b9921e3e67a0a1cbae6cf5f9165bcda6
Nicolas Gonzalez <nikolasgonzalez8@gmail.com>
2018-08-21 19:05:56 -0300

    Componente lista de pilotos

---
+++ src/app/piloto/piloto-list/piloto-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-piloto-list',
+  templateUrl: './piloto-list.component.html',
+  styleUrls: ['./piloto-list.component.css']
+})
+export class PilotoListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
