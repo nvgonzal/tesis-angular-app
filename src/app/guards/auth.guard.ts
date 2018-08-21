708297dd354f415dbb3ac794e7f496c20ee9398e
Nicolas Gonzalez <nikolasgonzalez8@gmail.com>
2018-08-21 19:04:28 -0300

    Arreglado error en constructor

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -8,7 +8,7 @@ import {AuthService} from '../services/auth.service';
 })
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService,
-              private router: Router)
+              private router: Router) { }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
