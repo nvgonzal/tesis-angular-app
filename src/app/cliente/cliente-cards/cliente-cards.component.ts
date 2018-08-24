86372124297b449f9c05f6fba37b67d66e1fa887
virtualghost4 <manarias@alumnos.ubiobio.cl>
2018-08-24 19:09:39 -0300

    adding style resources to main repo

---
+++ src/app/cliente/cliente-cards/cliente-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-cards',
+  templateUrl: './cliente-cards.component.html',
+  styleUrls: ['./cliente-cards.component.css']
+})
+export class ClienteCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/cliente/cliente-content/cliente-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-content',
+  templateUrl: './cliente-content.component.html',
+  styleUrls: ['./cliente-content.component.css']
+})
+export class ClienteContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
