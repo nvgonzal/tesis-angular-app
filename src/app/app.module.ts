da541c299fbca94c6a3c5524e8972e2d31018cfd
Nicolas Gonzalez <nikolasgonzalez8@gmail.com>
2018-08-29 22:51:46 -0300

    Revert "structure folder defined"
    This reverts commit 2da3cc3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,6 @@ import {FormsModule} from '@angular/forms';
 import {AuthGuard} from './guards/auth.guard';
 import { PilotoListComponent } from './piloto/piloto-list/piloto-list.component';
 import {AuthTokenInterceptor} from './helpers/auth.interceptor';
-import { ClienteComponent } from './cliente/cliente.component';
-import { ClientbuttonsComponent } from './cliente/clientbuttons.component';
-import { MainframeComponent } from './mainframe/mainframe.component';
-import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -22,11 +18,7 @@ import { HeaderComponent } from './header/header.component';
     LoginComponent,
     RegisterComponent,
     HomeComponent,
-    PilotoListComponent,
-    ClienteComponent,
-    ClientbuttonsComponent,
-    MainframeComponent,
-    HeaderComponent
+    PilotoListComponent
   ],
   imports: [
     BrowserModule,
