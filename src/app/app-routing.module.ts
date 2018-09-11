1e4a09865dfca7c12f4aeece860b6692f0c89624
Nicolas Gonzalez <nikolasgonzalez8@gmail.com>
2018-09-11 17:51:56 -0300

    Merge branch 'master' of https://github.com/nvgonzal/tesis-angular-app
    # Conflicts:
    #	src/app/app.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 import {AuthGuard} from './guards/auth.guard';
 import {PilotoListComponent} from './piloto/piloto-list/piloto-list.component';
-import {SysadminListComponent} from './sysadmin/sysadmin-list/sysadmin-list.component';
 import {DuenoListComponent} from './dueno/dueno-list/dueno-list.component';
 import {DuenoCreateComponent} from './dueno/dueno-create/dueno-create.component';
 import {DuenoHomeComponent} from './dueno/dueno-home/dueno-home.component';
@@ -19,7 +18,7 @@ const routes: Routes = [
   { path: 'sysadmin', component: SysadminListComponent},
   {
     path: 'dueno' , component : DuenoHomeComponent,
-    children :[
+    children : [
       { path: 'dueno-list' , component: DuenoListComponent},
       { path : 'dueno-edit' , component : DuenoEditComponent},
       { path : 'dueno-create' , component : DuenoCreateComponent}
