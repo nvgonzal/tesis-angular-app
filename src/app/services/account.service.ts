00465012bd7819e09cb59e67a73c3cf3db58738c
Nicolas Gonzalez <nikolasgonzalez8@gmail.com>
2018-09-11 17:30:35 -0300

    Crea guard de dueño

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs/index';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Accept' : 'application/json'})
@@ -15,7 +16,7 @@ export class AccountService {
   constructor(private http: HttpClient) { }
 
   public register(email: string, nombre: string, apellido_p: string, apellido_m: string,
-           rut: string, password: string, celular: string, fono: string) {
+           rut: string, password: string, celular: string, fono: string): Observable<any> {
     return this.http.post(this.url + '/register', {
       'email': email,
       'nombre': nombre,
