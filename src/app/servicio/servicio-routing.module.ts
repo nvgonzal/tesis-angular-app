207c6d22173a84d5ce98faffc58fc58f24cb509d
Nicolas Gonzalez <nikolasgonzalez8@gmail.com>
2018-10-12 22:01:14 -0300

    Agrega estilos y parchea vulnerabilidades

--- src/app/servicio/servicio-routing.module.ts
+++ src/app/servicio/servicio-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {ServicioHomeComponent} from './servicio-home/servicio-home.component';
 import {ServicioPagadoComponent} from './servicio-pagado/servicio-pagado.component';
 import {AuthGuard} from '../guards/auth.guard';
+import {WaitingPayComponent} from './waiting-pay/waiting-pay.component';
 
 const routes: Routes = [
   {path: '', component: ServicioHomeComponent, canActivate: [AuthGuard]},
   {path: 'pagado/:id', component: ServicioPagadoComponent, canActivate: [AuthGuard]},
+  {path: 'prueba', component: WaitingPayComponent}
 ];
 
 @NgModule({
